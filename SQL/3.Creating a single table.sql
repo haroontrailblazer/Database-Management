93015353211ad28aac967d4cf494bf703aa1e58d
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-29 16:37:35 +0530

    cret: table

---
+++ SQL/3.Creating a single table.sql
@@ -0,0 +1,7 @@
+-- cretiing a table coustmer
+CREATE TABLE coustmer (
+    coustmer_id INT PRIMARY KEY,
+    coustmer_name VARCHAR(50),
+    coustmer_address VARCHAR(50),
+    coustmer_phone INT
+);
