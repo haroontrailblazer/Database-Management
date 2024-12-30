ab35c6c09b62034d73ad5ee46354a593a6855a62
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-30 22:05:51 +0530

    synt

--- SQL/11.Adding a column in a table.sql
+++ SQL/11.Adding a column in a table.sql
@@ -1,2 +1,7 @@
 USE company;
 ALTER TABLE coustmer ADD phonenumber VARCHAR(20);
+
+/* 
+   syntax: ALTER TABLE table_name ADD column_name datatype;
+    Here, we are adding a column named phonenumber in the coustmer table.
+    */
