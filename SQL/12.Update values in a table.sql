30e616a73a3780211edef8befe5b4b577d79eeb3
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-30 22:07:09 +0530

    synt

--- SQL/12.Update values in a table.sql
+++ SQL/12.Update values in a table.sql
@@ -3,3 +3,8 @@ update coustmer set phonenumber='1234545346' where
 Cust_name='Ravi'; 
 update coustmer set phonenumber='45554654' where 
 cust_name='Raj';
+
+/* 
+  syntax:
+    update table_name set column_name='value' where column_name='value';
+*/
