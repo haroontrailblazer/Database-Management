7f36d8c1fa45e0d21e12068b4bea45db36de6901
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-30 21:45:55 +0530

    synt

--- SQL/3.Creating a single table.sql
+++ SQL/3.Creating a single table.sql
@@ -5,3 +5,20 @@ CREATE TABLE coustmer (
     coustmer_address VARCHAR(50),
     coustmer_phone INT
 );
+
+/* 
+  syntax for creating a table
+    CREATE TABLE table_name (
+        column1_name column1_datatype PRIMARY KEY,
+        column2_name column2_datatype NOT NULL,
+        column3_name column3_datatype  DEFAULT value,
+        ...
+
+        constrains:
+            PRIMARY KEY,
+            NOT NULL,
+            DEFAULT value,
+            UNIQUE,
+            CHECK,
+            FOREIGN KEY
+    );
