e9c0a4fe427c253308e3724439001fbd9449683b
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-30 21:54:48 +0530

    synt

--- SQL/8.Check what table inside.sql
+++ SQL/8.Check what table inside.sql
@@ -1,1 +1,9 @@
 select * from information_schema.tables
+
+/* 
+
+      syntax:
+            select * from information_schema.tables
+            where table_schema = 'your_database_name'
+            
+            */
