ef1ee71f96d9ddd9b3d903dff1af3e0019306971
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-31 08:39:25 +0530

    cmt

---
+++ SQL/23.Using camparison operator.sql
@@ -0,0 +1,7 @@
+select * from customer 
+where customerlastname > 'Brown' or customerlastname>'Cross'
+
+/* 
+    The above query will return all the rows where the customerlastname is greater than 'Brown' or 'Cross'
+    The result will be the same as the previous query
+*/
