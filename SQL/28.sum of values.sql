1b57c1a808b41aabcde89722c475d0f8367be1e2
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-31 09:06:26 +0530

    cmt

--- SQL/28.sum of values.sql
+++ SQL/28.sum of values.sql
@@ -4,3 +4,10 @@ SUM(salequantity) AS [Total Quantity]
 FROM sale, employee 
 WHERE sale.employeeid = employee.employeeid 
 GROUP BY sale.employeeid, EmployeeFirstName, EmployeeLastName;
+
+/* syntax:
+SELECT column_name(s)
+FROM table1
+JOIN table2
+ON table1.column_name = table2.column_name;
+*/
