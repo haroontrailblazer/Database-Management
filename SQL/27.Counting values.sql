2b4f057c127d51e7aa30dd2d831c14dbadc63ad9
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-31 09:05:25 +0530

    cmt

--- SQL/27.Counting values.sql
+++ SQL/27.Counting values.sql
@@ -1,3 +1,8 @@
 -- Returns the number of rows in the customer table where the first name starts with 'B'. 'AS' is used for aliasing, temporarily giving a name to a column or table.
 select count(*) from customer  
 where CustomerFirstName like'B%';
+
+/* syntax:
+SELECT COUNT(*) FROM table_name
+WHERE column_name LIKE 'value%'; 
+*/
