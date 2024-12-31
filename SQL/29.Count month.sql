040c9cfce806561b741360740fe24a0385872faf
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-31 09:04:40 +0530

    cmt

--- SQL/29.Count month.sql
+++ SQL/29.Count month.sql
@@ -1,2 +1,13 @@
 select month(saledate) as [Month], count ( * ) as [Number of sale],sum(salequantity*saleunitprice) as [Total Amount] from sale 
 group by month(saledate);
+
+/* 
+     syntax:
+        select month(saledate) as [Month], count ( * ) as [Number of sale],sum(salequantity*saleunitprice) as [Total Amount] from sale
+        group by month(saledate);
+
+        month(saledate) as [Month] : to get the month from the saledate
+        count ( * ) as [Number of sale] : to get the number of sale
+        sum(salequantity*saleunitprice) as [Total Amount] : to get the total amount of sale
+        group by month(saledate) : to group the result by month
+*/
