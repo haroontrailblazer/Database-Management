0bd717bf60841d5f4af652dc5954e0028e2c6c65
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-31 08:41:18 +0530

    cmt

---
+++ SQL/24.Not equal condition.sql
@@ -0,0 +1,6 @@
+select * from customer 
+where customerlastname <> 'Brown';
+
+/* 
+    This query will return all the rows from the customer table where the customerlastname is not equal to 'Brown'
+*/
